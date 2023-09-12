54a7185c13555a88623a7403f14079f995debd39
P.POORANI <144570396+715DD3409@users.noreply.github.com>
2023-09-12 08:58:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# unit-1 1. factorial of given number
+
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*fact_rec(n-1)
+number =int(input("Enter a value: "))
+res=fact_rec(number)
+print("factorial of{} is {}.". format (number,res))
